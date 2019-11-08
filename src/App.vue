d36f025eaef64359e4a51741392139eeb79950d2
itning <itning@itning.top>
2019-11-08 16:17:42 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,143 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <a-locale-provider :locale="locale">
+    <a-layout style="height: 100%">
+      <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+        <div class="logo-box">
+          <img class="logo" :src="`${publicPath}logo.png`" alt="logo img"/>
+        </div>
+        <a-menu theme="dark" mode="inline" v-model="selectedMenuKeys">
+          <a-menu-item key="all_student" @click="pushRouter('/')">
+            <a-icon type="profile"/>
+            <span>学生查询</span>
+          </a-menu-item>
+          <a-menu-item key="review" @click="pushRouter('/review')">
+            <a-icon type="plus"/>
+            <span>新增学生</span>
+          </a-menu-item>
+          <a-menu-item key="plan_list" @click="pushRouter('/plan_list')">
+            <a-icon type="align-left"/>
+            <span>寝室查询</span>
+          </a-menu-item>
+          <a-menu-item key="export" @click="pushRouter('/export')">
+            <a-icon type="download"/>
+            <span>请假查询</span>
+          </a-menu-item>
+          <a-menu-item key="discount" @click="pushRouter('/discount')">
+            <a-icon type="edit"/>
+            <span>折扣管理</span>
+          </a-menu-item>
+          <a-menu-item key="office_review" @click="pushRouter('/office_review')">
+            <a-icon type="profile"/>
+            <span>我的审核</span>
+          </a-menu-item>
+          <a-menu-item key="buy" @click="pushRouter('/buy')">
+            <a-icon type="profile"/>
+            <span>我的购书</span>
+          </a-menu-item>
+          <a-menu-item key="add" @click="pushRouter('/add')">
+            <a-icon type="edit"/>
+            <span>填写购书信息</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="()=> collapsed = !collapsed"
+          />
+          <a-dropdown class="user-btn">
+            <a class="ant-dropdown-link">
+              {{loginUser}}
+              <a-icon type="down"/>
+            </a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a @click="logout">注销登录</a>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </a-layout-header>
+        <a-layout-content>
+          <router-view/>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-locale-provider>
 </template>
+<script>
+    import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
+
+    export default {
+        name: 'App',
+        data: () => ({
+            publicPath: process.env.BASE_URL,
+            locale: zhCN,
+            //关闭菜单栏
+            collapsed: false,
+            //登录用户
+            loginUser: '',
+            //当前选中菜单
+            selectedMenuKeys: ['all_student'],
+        }),
+        methods: {
+            /**
+             * 更改路由
+             * @param path 路由路径
+             */
+            pushRouter(path) {
+                this.$router.push(path);
+            },
+            /**
+             * 注销登录
+             */
+            logout() {
+                window.localStorage.removeItem('authorization_token');
+                window.location.href = CAS_LOGOUT_URL;
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
+<style scoped>
+  /*调整菜单栏开关按钮位置和鼠标样式*/
+  .trigger {
+    padding: 20px;
+    cursor: pointer;
+  }
+
+  /*logo盒子*/
+  .logo-box {
+    padding: 0 10px;
+    line-height: 64px;
+    height: 64px;
+    overflow: hidden;
+  }
+
+  .logo-box img {
+    width: 180px;
+  }
+
+  /*用户操作按钮*/
+  .user-btn {
+    padding-right: 24px;
+    float: right;
+  }
+</style>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  /*内容*/
+  .content-style {
+    margin: 24px 16px;
+    padding: 24px;
+    background: #fff;
+    min-height: 280px;
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .ant-table-fixed-right {
+    z-index: 999;
+  }
 </style>
---
+++ src/views/AllStudent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    a
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "AllStudent"
+    }
+</script>
+
+<style scoped>
+
+</style>
